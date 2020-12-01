621afcb1c8ca974f07aa8ceb7a3a261efbc9e6b0
DieZ <diez.eel@gmail.com>
2020-12-01 04:11:31 +0200

    fix

--- src/CIServiceProvider.php
+++ src/CIServiceProvider.php
@@ -11,22 +11,7 @@ class CIServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $configPath = __DIR__ . '/../config';
-        $this->publishes([
-            $configPath.'/autoload.php' => app_path('CI/config/autoload.php'),
-            $configPath.'/config.php' => app_path('CI/config/config.php'),
-            $configPath.'/constants.php' => app_path('CI/config/constants.php'),
-            $configPath.'/doctypes.php' => app_path('CI/config/doctypes.php'),
-            $configPath.'/foreign_chars.php' => app_path('CI/config/foreign_chars.php'),
-            $configPath.'/hooks.php' => app_path('CI/config/hooks.php'),
-            $configPath.'/memcached.php' => app_path('CI/config/memcached.php'),
-            $configPath.'/migration.php' => app_path('CI/config/migration.php'),
-            $configPath.'/mimes.php' => app_path('CI/config/mimes.php'),
-            $configPath.'/profiler.php' => app_path('CI/config/profiler.php'),
-            $configPath.'/routes.php' => app_path('CI/config/routes.php'),
-            $configPath.'/smileys.php' => app_path('CI/config/smileys.php'),
-            $configPath.'/user_agents.php' => app_path('CI/config/user_agents.php'),
-        ], 'config');
+        $this->registerConfig();
     }
 
     /**
@@ -34,44 +19,20 @@ class CIServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->registerServices();
+        //
     }
 
     /**
-     * Register all modules.
+     * Register package's namespaces.
      */
-//    protected function registerModules()
-//    {
-//        $this->app->register(BootstrapServiceProvider::class);
-//    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function registerServices()
+    protected function registerConfig()
     {
-//        $this->app->singleton(Contracts\CiInterface::class, function ($app) {
-//            return new BootstrapCI($app);
-//        });
-//        $this->app->alias(Contracts\CiInterface::class, 'ci');
-    }
+        $configPath = __DIR__ . '/../config/config.php';
 
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        //return [Contracts\CiInterface::class, 'ci'];
+        $this->mergeConfigFrom($configPath, 'ci');
+        $this->publishes([
+            $configPath => config_path('ci.php'),
+        ], 'config');
     }
-
-    /**
-     * Register providers.
-     */
-    protected function registerProviders()
-    {
-//        $this->app->register(ConsoleServiceProvider::class);
-//        $this->app->register(ContractsServiceProvider::class);
-    }
+ 
 }
